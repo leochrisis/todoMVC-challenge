3b26664922df5ae25b8f428af123beaf91b9dc93
leochrisis <leonardo.chfc@gmail.com>
2018-03-21 10:10:41 -0300

    Add application's home page

---
+++ src/pages/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <q-toolbar color="primary">
+      <q-toolbar-title>
+        ToDo MVC
+      </q-toolbar-title>
+      <q-btn flat round dense icon="add" size="25px"/>
+    </q-toolbar>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'Home'
+}
+</script>
