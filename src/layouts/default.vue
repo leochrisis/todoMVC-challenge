244e80740fe0513b308e15bc304e8518e13f52b5
leochrisis <leonardo.chfc@gmail.com>
2018-03-22 10:02:05 -0300

    Add footer to default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,6 +3,15 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+    <q-layout-footer>
+      <q-toolbar color="primary" inverted>
+        <q-toolbar-title>
+          <span slot="subtitle">
+            Application made with <a href="http://quasar-framework.org/" target="blanck">Quasar Framework</a>. For more informations acess the <a href="https://github.com/leochrisis/todoMVC-challenge" target="blanck">Github</a> repository
+          </span>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-layout-footer>
   </q-layout>
 </template>
 
